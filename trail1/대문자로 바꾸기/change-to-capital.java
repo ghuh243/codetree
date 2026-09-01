72effd083794662434ce3c37f16b5f2cff0d9515
HanJin Ryu <136077253+ghuh243@users.noreply.github.com>
2026-09-01 10:47:21 +0900

    [Passed] 대문자로 바꾸기 (120ms, 10MB)

---
+++ trail1/대문자로 바꾸기/change-to-capital.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        char[][] arr = new char[5][3];
+
+        for (int i =0; i<5; i++) {
+            for (int j = 0; j <3; j++) {
+                arr[i][j] = sc.next().charAt(0);
+            }
+        }
+
+        for (int i =0; i<5; i++) {
+            for (int j = 0; j <3; j++) {
+                System.out.print((char)(arr[i][j]-32) + " ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
